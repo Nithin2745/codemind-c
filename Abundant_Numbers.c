6a61680292c090238a3ebf4195f88cca86554378
Nithin2745 <147487470+Nithin2745@users.noreply.github.com>
2024-01-02 23:11:24 +0530

    #include<stdio.h> int main() {     int n,i=1,res=0;     scanf("%d",&n);     while(i<n)     {         if(n%i==0)         {             res = res+i;         }         i++;     }     if(res>n)     {         printf("True");     }     else     {         printf("False");     } }

---
+++ Abundant_Numbers.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int n,i=1,res=0;
+    scanf("%d",&n);
+    while(i<n)
+    {
+        if(n%i==0)
+        {  
+            res = res+i;
+        }
+        i++;
+    }
+    if(res>n)
+    {
+        printf("True");
+    }
+    else
+    {
+        printf("False");
+    }
+}
